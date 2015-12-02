7d656d37c681adebb6ba4084269d272efbc50dba
Aydemphia <aydemphia@gmail.com>
2015-12-02 12:43:37 +0100

    tp finit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctypte html>
+<html>
+<head>
+
+	<title>Recette - Burger Maison</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="global.css">
+
+</head>
+<body>
+
+
+
+	<div id="global">
+
+		<div id="insideLeft">
+
+			<h1 class="title">Burger Maison</h1>
+
+			<p id="auteur">
+
+				par <a href="#">Vincent</a> - Note: 4.7
+
+			</p>
+
+			<p id="more">
+
+				<a href="#">Voir d'autres recettes...</a>
+
+			</p>
+
+		</div>
+
+		<div id="insideRight">
+
+			<div id="infos">
+
+				<div><img src="images/icon-timer.png" class="icon" alt="Temps" /><br />
+				<span class="value">35</span><span class="miniText">min</span></div>
+
+				<div><img src="images/icon-calorie.png" class="icon" alt="Calories" /><br />
+				<span class="value">540</span><span class="miniText">kcal</span></div>
+
+				<div><img src="images/icon-billet.png" class="icon" alt="Temps" /><br />
+				<span class="value">10</span><span class="miniText">&euro;</span></div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+
+
+</body>
+</html>
